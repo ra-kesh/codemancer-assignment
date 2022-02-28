c3c1c94f4ee82d2e11c39ab7c97ee61394c654a3
Rakesh <iamrakeshkumar@pm.me>
2022-02-28 14:34:47 +0530

    refactor:gif container

---
+++ src/components/GifContainer/GifContainer.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Styles from "./GifContainer.module.css";
+
+const GifContainer = ({ setGifUrl }) => {
+  const [searchText, setSearchText] = useState("");
+  const [gifData, setGifData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log(gifData);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+
+        const {
+          data: { data },
+        } = await axios("http://api.giphy.com/v1/gifs/search", {
+          params: {
+            api_key: process.env.REACT_APP_GIPHY_API_KEY,
+            q: searchText,
+            limit: 25,
+          },
+        });
+
+        setGifData(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
+    })();
+  }, [searchText]);
+
+  return (
+    <div className={Styles.container}>
+      <input
+        type="text"
+        value={searchText}
+        onChange={(e) => setSearchText(e?.target?.value)}
+      />
+      <div className={Styles.gifs}>
+        {!isLoading &&
+          gifData?.map((gif) => {
+            return (
+              <img
+                key={gif.id}
+                src={gif.images.fixed_height_small.url}
+                onClick={(e) => setGifUrl(e.target.src)}
+                alt=""
+                className={Styles.gif}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default GifContainer;
